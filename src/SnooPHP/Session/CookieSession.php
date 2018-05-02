f3334240ba2d931979a79202864e9e2e6672f50b
Sneppy <andrea.mecchia@mail.polimi.it>
2018-05-02 13:59:39 +0200

    Added null token

--- src/SnooPHP/Session/CookieSession.php
+++ src/SnooPHP/Session/CookieSession.php
@@ -6,6 +6,7 @@ namespace SnooPHP\Session;
  * A cookie-token based session
  * 
  * The user is required to have an id column
+ * Token is not require and can be set to null
  * 
  * @author Sneppy
  */
@@ -60,18 +61,18 @@ class CookieSession extends Session
 	 * 
 	 * @return bool false if fails to set some cookies
 	 */
-	public static function createSession($user, $token, $stayLogged = false)
+	public static function createSession($user, $token = null, $stayLogged = false)
 	{
 		/* @todo use field map */
 
-		$sessionMaxLength	= function_exists("sessionConfig") ? sessionConfig("session_max_length") : static::SESSION_MAX_LENGTH;
-		$sessionDefLength	= function_exists("sessionConfig") ? sessionConfig("session_def_length") : static::SESSION_DEF_LENGTH;
+		$sessionMaxLength	= function_exists("env") ? env("session_max_length") : static::SESSION_MAX_LENGTH;
+		$sessionDefLength	= function_exists("env") ? env("session_def_length") : static::SESSION_DEF_LENGTH;
 		$hashField			= static::$tokenFieldMap["hash"];
 		$expirationField	= static::$tokenFieldMap["expires_at"];
 		// Calc expiration date
 		$expiration = $stayLogged ?
 		time() + $sessionMaxLength : (
-			$token->$expirationField ?
+			$token !== null && $token->$expirationField ?
 			strtotime($token->$expirationField) :
 			time() + $sessionDefLength
 		);
@@ -81,7 +82,7 @@ class CookieSession extends Session
 		// Store user as a json string
 		$status &= setcookie("session_user", to_json($user), $expiration, "/");
 		// Store token hash
-		$status &= setcookie("session_token", $token->$hashField, $expiration, "/");
+		if ($token) $status &= setcookie("session_token", $token->$hashField, $expiration, "/");
 		// Store stay logged flag
 		$status &= setcookie("session_time", $expiration - time(), $expiration, "/");
 
@@ -98,7 +99,7 @@ class CookieSession extends Session
 		
 		setcookie("session_id", $_COOKIE["session_id"], $expiration, "/");
 		setcookie("session_user", $_COOKIE["session_user"], $expiration, "/");
-		setcookie("session_token", $_COOKIE["session_token"], $expiration, "/");
+		if (static::token()) setcookie("session_token", $_COOKIE["session_token"], $expiration, "/");
 		setcookie("session_time", $_COOKIE["session_time"], $expiration, "/");
 	}
 
